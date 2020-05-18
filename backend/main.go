c666195b219a0b611fabbb698833e5743d4cec23
tarou <r.madrid.pg@gmail.com>
2020-05-18 23:39:27 +0900

    0518

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
 )
-func getRequest(){
+
+type menu struct {
+	id int
+	name string
+	price int
+	text string
+}
+
+
+func getRequest () {
 	r := gin.Default()
 	r.GET("/ping", func(c * gin.Context){
 		c.JSON(200, gin.H{
@@ -13,4 +22,13 @@ func getRequest(){
 		})
 	})
 	fmt.Printf("Server Side")
+	r.Run()
+}
+
+func getMenu () {
+
+}
+
+func writeReserve () {
+
 }
