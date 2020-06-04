4a0763df67658687371a48f86c04d2507640c259
tarou <r.madrid.pg@gmail.com>
2020-06-05 00:33:50 +0900

    0605

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
     "net/http"
 )
 
+// メニュー構造体
 type menu struct {
 	id int
 	name string
@@ -32,6 +33,51 @@ func main () {
 var store = map[string] *menu{}
 var lock = sync.RWMutex{}
 
+// メニュー取得
 func getAllMenu(w rest.ResponseWriter, r *rest.Request) {
 	
+	lock.RLock()
+	menus := make([]menu, len(store))
+	i := 0
+	for _, m := range store {
+		menus[i] = *m
+		i++
+	}
+	lock.RUnlock()
+	w.WriteJson(&menus)
+}
+
+// メニュー追加
+// メニュー削除
+func deleteMenu (w rest.ResponseWriter, r *rest.Request) {
+	code := r.PathParam("id")
+	lock.Lock()
+	delete(store, code)
+	lock.Unlock()
+	w.WriteHeader(http.StatusOK)
+}
+
+var storeOrder = map[int] *order{}
+var lockOrder = sync.RWMutex{}
+
+// 注文の構造体
+type order struct {
+	Id int
+	Name string
+	Amount int
+	Price int
+	Date string
+}
+// オーダー登録
+func write(w rest.ResponseWriter, r *rest.Request){
+	orders := order{}
+	err := r.DecodeJsonPayload(&orders)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	lock.Lock()
+	storeOrder[orders.Id] = &orders
+	lockOrder.Unlock()
+	w.WriteJson(&orders)
 }
